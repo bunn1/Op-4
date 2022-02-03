6f90266331b1ac99c31221db11f2f64bfa814bd8
bunn1 <nicjohbun@gmail.com>
2022-02-03 14:24:26 +0100

    rend list console

--- opp.js
+++ opp.js
@@ -1,16 +1,30 @@
 let url = "https:api.documenu.com/v2/restaurants/search/fields?state=ca?key=4c4b844c1d91dbd4522a4f2e0ac076a4"
 // console.log(url);
-
+const formElement = document.getElementById("searchForm")
+const cityInputElement = document.getElementById("cityInput")
+let restaurantList = []
 url = "data.json"
 console.log(url)
 
+formElement.addEventListener('submit', event => {
+    event.preventDefault()
+    console.log("hello.form", restaurantList.filter(rest => rest.address.city === "Petaluma" ))
+})
+
+// document.getElementById('restaurang').addEventListener('click', rendera =>{
+// function rendera(){
+//     document.getElementById("list").innerHTML = 
+
+// }
+
+
 fetch(url)
-    .then(function(response) {
+    .then(function (response) {
 
         return response.json()
-    }).then(function (obj){
+    }).then(function (obj) {
         console.log(obj.data)
-
+        restaurantList = obj.data
         obj.data.forEach(element => {
 
             //
@@ -21,14 +35,19 @@ fetch(url)
             listItem.appendChild(document.createTextNode(element.restaurant_name))
 
             list.appendChild(listItem)
-
-
             listItem.appendChild(listChild)
+
             listChild.textContent = element.cuisines
 
         })
 
     })
+
+
+
+
+
+
 
 // let resultRes =document.getElementById("result")
 
@@ -37,21 +56,21 @@ fetch(url)
 //         .then((response) => response.json()) 
 //         .then((obj) => {
 //              console.log(obj.data)
- 
+
 //         obj.data.forEach(element => {
 
 //              console.log(element.restaurant_name, element.restaurant_phone)
 
 
 //                 document.getElementById("result").innerHTML += `
-            
+
 //                 <p>${element.restaurant_name}</p>
 //                 <p>${element.restaurant_phone}</p>
 //                 </div>
 //                 `
 
 //             })
-            
+
 //         })
 
 // }

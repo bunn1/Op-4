0c7f6116c184e01da68efd2388ce3476b3f2ea13
bunn1 <nicjohbun@gmail.com>
2022-02-04 14:20:20 +0100

    filtrera citys, clear button

--- opp.js
+++ opp.js
@@ -1,46 +1,105 @@
+// Hämtar url
 let url = "https:api.documenu.com/v2/restaurants/search/fields?state=ca?key=4c4b844c1d91dbd4522a4f2e0ac076a4"
-// console.log(url);
+
+// Fiskar upp element
 const formElement = document.getElementById("searchForm")
 const cityInputElement = document.getElementById("cityInput")
-let restaurantList = []
+
+// Tom lista
+let restaurantList = [];
 url = "data.json"
 console.log(url)
 
-formElement.addEventListener('submit', event => {
-    event.preventDefault()
-    console.log("hello.form", restaurantList.filter(rest => rest.address.city === "Petaluma" ))
-    console.log("hello.form", restaurantList.filter(rest => rest.address.city === "Pico Rivera" ))
-    console.log("hello.form", restaurantList.filter(rest => rest.address.city === "Perris" ))
-    
-})
 
-// document.getElementById('restaurang').addEventListener('click', rendera =>{
-// function rendera(){
-//     document.getElementById("list").innerHTML = 
+let list =document.getElementById("list");
 
+// Lista med strängar - städer
+const objektCity = [ 'Petaluma',  'Pico Rivera', 'Perris']
+
+const mapFunc = (element) => element.restaurant_name;
+
+const mappedObject = objektCity.map(mapFunc)
+
+
+// const objektCity = [{restaurant:adress.city}]
+// function getCity(adress, city) {
+//     let objektCity = [adress.city].join("")
+//     return objektCity;
 // }
 
+// Anonym funktion för click submit
+formElement.addEventListener('submit', event => {
+    event.preventDefault()
+    // console.log(cityInput)
 
+    // filter restuants blir en ny array som bara innehåller rest du vill ha  
+    const filterrestaurants = restaurantList.filter(rest => {
+
+        // Konvertera till småbokstäver
+        return rest.address.city.toLowerCase() === cityInputElement.value.toLowerCase() })
+
+
+
+    const mappedRestaurants = filterrestaurants.map(mapFunc)
+
+    // list.innerHTML = mappedRestaurants
+
+    // Rensar ut HTML
+    list.innerHTML = "";
+
+    // mapped Rest loop
+    filterrestaurants.forEach(rest => {
+
+        console.log(rest)
+
+        // skapa HTML enligt en mall
+        let tagString = `<li>
+        <h3>${rest.restaurant_name}</h3>
+        <hr>
+        <h5>${rest.address.city}</h5>
+        <h5>${rest.restaurant_phone}</h5>
+        </li>`
+
+        //Tryck in strängen i list
+        list.innerHTML += tagString;
+
+    })
+
+})
+
+let rensa = document.getElementById("clear");
+
+rensa.addEventListener("click", () => {
+
+    list.innerHTML = "";
+})
+
+
+//Hämtar upp info API 
 fetch(url)
     .then(function (response) {
 
+        // Tar resursen och konverterar till json data
         return response.json()
+
+        // Tar emot responsen
+
     }).then(function (obj) {
         console.log(obj.data)
         restaurantList = obj.data
         obj.data.forEach(element => {
 
             //
-            let list = document.getElementById("list");
-            let listItem = document.createElement("li");
-            let listChild = document.createElement("p");
+            // let list = document.getElementById("list");
+            // let listItem = document.createElement("li");
+            // let listChild = document.createElement("p");
 
-            listItem.appendChild(document.createTextNode(element.restaurant_name))
+            // listItem.appendChild(document.createTextNode(element.restaurant_name))
 
-            list.appendChild(listItem)
-            listItem.appendChild(listChild)
+            // list.appendChild(listItem)
+            // listItem.appendChild(listChild)
 
-            listChild.textContent = element.cuisines
+            // listChild.textContent = element.cuisines
 
         })
 
